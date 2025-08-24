f50e6466e7a67e0117ee1cfda9db668cfd9720b3
Serhii Holushkov <serhii.holushkov@gmail.com>
2025-08-23 22:17:45 -0400

    added documentation string to a first class

--- classes/first_class.py
+++ classes/first_class.py
@@ -1,9 +1,12 @@
 class FirstClass:
+    """A simple example class."""
+
     def __init__(self, prop1, prop2):
         self.prop1 = prop1
         self.prop2 = prop2
 
 
 instance = FirstClass("value1", "value2")
+print(FirstClass.__doc__)
 print(instance.prop1)
 print(instance.prop2)
