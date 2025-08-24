9b0b326752f2ddf3ffeae062f16536792216a40a
Serhii Holushkov <serhii.holushkov@gmail.com>
2025-08-23 22:16:17 -0400

    first class example

---
+++ classes/first_class.py
@@ -0,0 +1,9 @@
+class FirstClass:
+    def __init__(self, prop1, prop2):
+        self.prop1 = prop1
+        self.prop2 = prop2
+
+
+instance = FirstClass("value1", "value2")
+print(instance.prop1)
+print(instance.prop2)
