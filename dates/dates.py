dc144171fddf5c980158323ddf66bca2456e1f02
Serhii Holushkov <serhii.holushkov@gmail.com>
2025-08-28 22:34:46 -0400

    short intro to dates

---
+++ dates/dates.py
@@ -0,0 +1,13 @@
+import datetime
+
+date = datetime.date(2025, 1, 25)
+
+print(date)
+today = datetime.date.today()
+print(today)
+
+
+time = datetime.time(14, 30, 0)
+print(time)
+now = datetime.datetime.now()
+print(now)
